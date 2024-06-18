b22743f9664a55dbff4fb69bb3b48b45a4d062e1
Rostislav Parahnenko <asawww100@gmail.com>
2024-06-18 13:04:37 +0300

    upd

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+// import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  // <Provider>
+  <App />
+  // </Provider>
 );
