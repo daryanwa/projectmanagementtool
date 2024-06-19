14905f602337f696015895388bbeddfe5de08c88
Rostislav <asawww100@gmail.com>
2024-06-19 15:24:26 +0300

    last upd

--- src/components/Login/AuthForm.tsx
+++ src/components/Login/AuthForm.tsx
@@ -35,9 +35,14 @@ const AuthForm: React.FC<AuthFormProps> = ({
             placeholder="example@mail.com"
             className="  p-[calc(0.5em-1px)_calc(0.75em-1px)] text-lg h-[45px] bg-surface  outline-none rounded shadow-sm "
           />
+          {error && (
+            <p className="text-red-500 mt-2 min-w-60">
+              Email or password is incorrect!
+            </p>
+          )}
         </div>
       </div>
-      <div className="mt-4">
+      <div className="mt-2">
         <div className="relative flex flex-col text-lg font-normal">
           <label>Password</label>
           <input
@@ -49,17 +54,28 @@ const AuthForm: React.FC<AuthFormProps> = ({
             placeholder="Enter your password"
             className="p-[calc(0.5em-1px)_calc(0.75em-1px)] text-lg h-[45px] bg-surface outline-none rounded shadow-sm"
           />
+          {error && (
+            <p className="text-red-500 min-w-60">
+              Email or password is incorrect!
+            </p>
+          )}
         </div>
       </div>
-      <button className=" min-h-auto min-w-40 mt-4 mb-6 items-start flex w-32 hover:text-primary font-semibold ">
-        {select === "login" ? <p>Forgot Password?</p> : <p></p>}
-      </button>
+
+      {select === "login" ? (
+        <button className=" min-h-auto min-w-40 mt-4 mb-8 items-start flex w-32 hover:text-primary font-semibold ">
+          {" "}
+          Forgot Password?{" "}
+        </button>
+      ) : (
+        <p className="mb-4"></p>
+      )}
 
       {select === "login" ? (
         <button
           onClick={handleSignIn}
           type="submit"
-          className="bg-secondary h-10 rounded hover:bg-slate-400 hover:shadow-sm font-bold shadow-md hover:bg-gradient-to-t  hover:from-white hover:to-slate-100"
+          className="bg-secondary mt-2 h-10 rounded hover:bg-slate-400 hover:shadow-sm font-bold shadow-md hover:bg-gradient-to-t  hover:from-white hover:to-slate-100"
         >
           Login{" "}
         </button>
@@ -67,7 +83,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
         <button
           onClick={handleSignUp}
           type="submit"
-          className="bg-secondary h-10 rounded hover:bg-slate-400 hover:shadow-sm font-bold shadow-md hover:bg-gradient-to-t  hover:from-white hover:to-slate-100"
+          className="bg-secondary mt-2 h-10 rounded hover:bg-slate-400 hover:shadow-sm font-bold shadow-md hover:bg-gradient-to-t  hover:from-white hover:to-slate-100 "
         >
           SignUp{" "}
         </button>
--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -37,6 +37,7 @@ function LoginComponentNew() {
   useEffect(() => {
     setEmail("");
     setPassword("");
+    setError(null);
   }, [select]);
 
   return (
