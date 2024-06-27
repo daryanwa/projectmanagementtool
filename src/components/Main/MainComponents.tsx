8dd9c5401d5843a8fec951bd5a9ca575a816d031
Rostislav <asawww100@gmail.com>
2024-06-27 18:49:38 +0300

    noteupd

--- src/components/Main/MainComponents.tsx
+++ src/components/Main/MainComponents.tsx
@@ -5,9 +5,14 @@ import UserProfileComponent from "./UserProfile/UserProfileComponent";
 import NoteComponent from "./Note/NoteComponent";
 import BottomButton from "./BottomButtons.tsx/BottomButton";
 import { getAuth, signOut } from "firebase/auth";
+import { getCurrentUserId } from "../../api/getCurrentUser";
+import { NoteInterface } from "../../interfaces/noteInterfaces";
+import { SaveNote } from "../../services/userSaveToDb";
 
 function MainComponents() {
   const [logOut, setLogOut] = useState<boolean>(false);
+  const [allNotes, setAllNotes] = useState<NoteInterface[]>([]);
+  const saveNoteButton = new SaveNote();
   const handleSignOut = async () => {
     try {
       const auth = getAuth(app);
@@ -17,13 +22,62 @@ function MainComponents() {
       throw new Error(err.message);
     }
   };
+
+  const handleCreate = async (text: string) => {
+    const userId = await getCurrentUserId();
+    console.log(text);
+    const newNote: NoteInterface = {
+      id: Date.now().toString(),
+      text: text,
+    };
+    try {
+      await saveNoteButton.saveNote(userId, newNote);
+      // setAllNotes([...allNotes, newNote]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleCreateNote = async () => {
+    const userId = await getCurrentUserId();
+
+    const newNote: NoteInterface = {
+      id: Date.now().toString(),
+      text: "",
+    };
+    try {
+      await saveNoteButton.saveNote(userId, newNote);
+      setAllNotes([...allNotes, newNote]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchNote = async () => {
+    try {
+      const userId = await getCurrentUserId();
+      const noteCollection = collection(db, "notes", userId);
+      const notesSnapshot = await getDocs(noteCollection);
+      const notesList = notesSnapshot.docs.map(
+        (doc) => doc.data() as NoteInterface
+      );
+      setAllNotes(notesList);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNote(); // Выполняем загрузку заметок при монтировании компонента
+  }, []);
+
   return (
-    <div className="bg-orange-200 min-h-screen ">
+    <div className="bg-orange-200 min-h-screen  ">
       <div>
         <div>
           <div
             onClick={() => setLogOut(!logOut)}
-            className=" border-2 rounded-full w-14 h-14 flex absolute m-7 justify-center items-center bg-emerald-500 hover:scale-125 transition-transform end-10 text-white font cursor-pointer  mt-4  border-emerald-700">
+            className=" border-2 rounded-full w-14 h-14 flex absolute m-7 justify-center items-center bg-emerald-500 hover:scale-125 transition-transform end-10 text-white font cursor-pointer  mt-4  border-emerald-700"
+          >
             <div className="flex justify-center items-center w-full h-full absolute ">
               <UserProfileComponent />
             </div>
@@ -33,7 +87,8 @@ function MainComponents() {
                 <div className=" mt-2 mb-2 text-white">
                   <button
                     onClick={handleSignOut}
-                    className="hover:bg-slate-600 transition-all w-[2.9rem]  rounded-md text-[0.72rem] p-1">
+                    className="hover:bg-slate-600 transition-all w-[2.9rem]  rounded-md text-[0.72rem] p-1"
+                  >
                     Logout
                   </button>
                 </div>
@@ -42,8 +97,10 @@ function MainComponents() {
           </div>
         </div>
       </div>
-      <NoteComponent />
-      <BottomButton />
+      {allNotes.map((note) => (
+        <NoteComponent key={note.id} note={note} onSave={handleCreate} />
+      ))}
+      <BottomButton handleCreate={handleCreateNote} />
     </div>
   );
 }
--- src/components/Main/Note/NoteComponent.tsx
+++ src/components/Main/Note/NoteComponent.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { NoteInterface } from "../../../interfaces/noteInterfaces";
 
-const NoteComponent = () => {
+interface NoteProps {
+  note: NoteInterface;
+  onSave: (text: string) => void;
+}
+
+const NoteComponent: React.FC<NoteProps> = ({ note, onSave }) => {
+  const [text, setText] = useState<string>(note.text);
+  const handleSave = () => {
+    onSave(text);
+  };
   return (
     <div className="flex">
-      <textarea className="border-4 min-w-[10] min-h-[12rem] bg-slate-300 m-8 border-gray-400 resize"></textarea>
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onBlur={handleSave}
+        className="border-4 min-w-[10] min-h-[12rem] bg-slate-300 m-8 border-gray-400 resize"
+      ></textarea>
     </div>
   );
 };
